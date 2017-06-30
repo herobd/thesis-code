6ee8e4511264083e3688a8d1fed15c326cb00752
Brian Davis <herobd@gmail.com>
2017-06-30 13:43:57 -0600

    Fixed errors

--- processSimResults.py
+++ processSimResults.py
@@ -13,10 +13,10 @@ for i in range(1,len(sys.argv)):
         for row in reader:
             data.append(row)
         endString = data[-1][27]
-        manualEnd = 'MANUAL' in endString
-        blankEnd = 'BLANK' in endString
-        if manualEnd != blankEnd:
-            outRow.append('nonstandard ending')
+        manualEnd = ('MANUAL' in endString)
+        blankEnd = ('BLANK' in endString)
+        if not manualEnd != blankEnd:
+            outRow.append('nonstandard ending: '+endString)
         elif len(data)>1:
 
             startTime = (int(data[1][0][-8:-6])*60 + int(data[1][0][-5:-3]))*60 + int(data[1][0][-2:])
@@ -26,17 +26,20 @@ for i in range(1,len(sys.argv)):
                     endIndex-=1
                     
             endTime = (int(data[endIndex][0][-8:-6])*60 + int(data[endIndex][0][-5:-3]))*60 + int(data[endIndex][0][-2:])
+            time = endTime-startTime
+            if time<0:
+                time += 24*60*60
             endCompletion = float(data[endIndex][2])
 
             transSent=0
             trueDone=0
             falseDone=0
             for index in range(1,len(data)+endIndex+1):
-                transSent += int(data[endIndex][9])
-                trueDone += int(data[endIndex][12])
-                falseDone += int(data[endIndex][13])
+                transSent += int(data[index][9])
+                trueDone += int(data[index][12])
+                falseDone += int(data[index][13])
 
-            compsec = endCompletion/(endTime-startTime)
+            compsec = endCompletion/(time)
             outRow.append(endCompletion)
             outRow.append(compsec*60*60*24)
             outRow.append(transSent)
