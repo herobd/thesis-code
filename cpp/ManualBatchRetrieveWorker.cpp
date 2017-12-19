cbb70eac5d01ee25bc0e7051389712de86fec9a4
Brian Davis <herobd@gmail.com>
2017-12-19 11:48:00 -0700

    Forgotten file

---
+++ cpp/ManualBatchRetrieveWorker.cpp
@@ -0,0 +1,44 @@
+//#include "ManualBatchRetrieveWorker.h"
+
+#include <nan.h>
+#include <iostream>
+#include <assert.h>
+
+#include "CATTSS.h"
+#include "BatchWraper.h"
+using namespace Nan;
+using namespace std;
+using namespace v8;
+
+class ManualBatchRetrieveWorker : public AsyncWorker {
+    public:
+        ManualBatchRetrieveWorker(Callback *callback, CATTSS* cattss, int width, bool line)
+        : AsyncWorker(callback), width(width), cattss(cattss), line(line) {}
+
+
+        ~ManualBatchRetrieveWorker() {}
+
+
+        void Execute () {
+            if (line)
+                batch = cattss->getLineBatch(width);
+            else
+                batch = cattss->getManualBatch(width);
+        }
+
+        // We have the results, and we're back in the event loop.
+        void HandleOKCallback () {
+            batch->doCallback(callback);
+            delete batch;
+        }
+    private:
+        //output
+        BatchWraper* batch;
+        //input
+        int width;
+        CATTSS* cattss;
+        bool line;
+        
+};
+
+
